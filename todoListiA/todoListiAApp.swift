dd79ab31a6a41b58568469b98c37dbb6fef06b90
canknbr <cankanbur1@gmail.com>
2023-05-15 16:41:08 +0300

    Initial Commit

---
+++ todoListiA/todoListiAApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoListiAApp.swift
+//  todoListiA
+//
+//  Created by Can Kanbur on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct todoListiAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
